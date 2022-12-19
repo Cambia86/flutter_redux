60e2329a139245e6ff0ca89f0f55c0114fcd2920
Alessandro <alessandrocambiaghi@gmail.com>
2022-12-19 17:33:35 +0100

    login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_redux_project/redux/model/app_state.dart';
 
 import 'drawer_widget.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _LoginPageState();
-  }
-}
-
-class _LoginPageState extends State<LoginPage> {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
+    return StoreConnector<AppState,AppState>(
+      onInit: (store) => {
+        // store.dispatch(const )
+      },
+      converter: (store) => store.state,
+      builder: (BuildContext context,AppState state){
+        
+        return Scaffold(
+            appBar: AppBar(
+        title: Text("widget.title"),
       ),
       drawer: drawer_widget(),
-      body:Container());
+      body: Container(child: Text("login page"),));
+      } ,
+    );
   }
 }
